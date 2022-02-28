67d09fd65821334652f1a438d73b239ed8033d15
Dalton Baker <dalton.s.baker@gmail.com>
2022-02-28 01:54:50 -0700

    Updated apache config files and ssl to add dns record

--- user_folder/ssl_setup.sh
+++ user_folder/ssl_setup.sh
@@ -4,7 +4,10 @@
 a2enmod ssl
 
 #copy apache config file
-cp /home/usr/ssl.conf /etc/apache2/sites-enabled/ssl.conf
+#cp /home/usr/drdoomsalot-music.com.conf /etc/apache2/sites-enabled/drdoomsalot-music.com.conf
+#cp /home/usr/drdoomsalot-music.com-le-ssl.conf /etc/apache2/sites-enabled/drdoomsalot-music.com-le-ssl.conf
+#cp /home/usr/music.daltonsbaker.com.conf /etc/apache2/sites-enabled/music.daltonsbaker.com.conf
+#cp /home/usr/music.daltonsbaker.com-le-ssl.conf /etc/apache2/sites-enabled/music.daltonsbaker.com-le-ssl.conf
 
 #restart apache, for some reason the restart command doesn't work
 service apache2 stop
