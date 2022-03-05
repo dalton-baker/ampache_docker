73e48104b79c7f20d94e3173f778f2d4ac5f3e86
Dalton Baker <dalton.s.baker@gmail.com>
2022-03-05 02:39:51 -0700

    Updated restore script to match the backup script

--- backups/scripts/restore_script.sh
+++ backups/scripts/restore_script.sh
@@ -8,25 +8,22 @@ tar xf $1 --strip 1
 
 echo "      - restoring sql database"
 cd /var/lib/mysql
-tar xf /backup/sql-backup.tar --strip 3
+tar xf /backup/partial/sql-backup.tar --strip 3
 
 echo "      - restoring sql config"
 cd /etc/mysql
-tar xf /backup/sql-config-backup.tar --strip 2
+tar xf /backup/partial/sql-config-backup.tar --strip 2
 
 echo "      - restoring Ampache config"
 cd /var/www/config
-tar xf /backup/config-backup.tar --strip 3
+tar xf /backup/partial/config-backup.tar --strip 3
 
 echo "      - restoring Ampache logs"
 cd /var/log/ampache
-tar xf /backup/logs-backup.tar --strip 3
+tar xf /backup/partial/logs-backup.tar --strip 3
 
 #removed unpacked single file backups
-rm /backup/sql-backup.tar
-rm /backup/sql-config-backup.tar
-rm /backup/config-backup.tar
-rm /backup/logs-backup.tar
+rm -r /backup/partial
 
 
 echo "   Finished restoring Ampache"
